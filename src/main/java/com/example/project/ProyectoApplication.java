6971afe1443af2a18e882d13cbe4aa518e8576a0
Elias <andres.carrascal@bpt.com.co>
2022-09-29 20:34:47 -0500

    Cambios para el deploy

--- src/main/java/com/example/project/ProyectoApplication.java
+++ src/main/java/com/example/project/ProyectoApplication.java
@@ -3,8 +3,6 @@ package com.example.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class ProyectoApplication {
 
